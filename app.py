46cfd160054af9e435421b36a9b86e193e9f32b4
Ricardo José Diaz Briceño <rjoseb11@gmail.com>
2019-04-04 12:27:27 -0500

    Slight changes to indentation

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ model = utils.Model("models/")
 @app.route('/predict/',methods=['GET','POST'])
 def predict():
     # Obtain the data from the request
-	data = request.get_data()
+    data = request.get_data()
     # Runs the model and returns the outputs in a json format
-	output = model.model_predict(data)
+    output = model.model_predict(data)
 
-	return output	
+    return output
 
 if __name__ == "__main__":
     # Running the Flask app on the url http://0.0.0.0:7000/
     # Use 0.0.0.0 to run in any IP available
-	app.run(host='0.0.0.0', port=7000, threaded=True)
+    app.run(host='0.0.0.0', port=7000, threaded=True)
