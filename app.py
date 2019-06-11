ae990cb34657fca84a97de36e6e9c9e494a99701
Ricardo Diaz <rjoseb11@gmail.com>
2019-06-11 15:09:05 -0500

    changed API to use POST requests

--- app.py
+++ app.py
@@ -10,12 +10,8 @@ model = utils.Model("models/")
 model.init_model()
 
 
-<<<<<<< HEAD
 # The model runs in the /predict route
 @app.route('/predict', methods=['POST'])
-=======
-@app.route('/predict/', methods=['GET', 'POST'])
->>>>>>> 7c4c5a9f80ca19a2024f33718e7f93d73dca4445
 def predict():
     # Obtain the data from the request
     data = request.get_data()
@@ -26,8 +22,4 @@ def predict():
 if __name__ == "__main__":
     # Running the Flask app on the url http://0.0.0.0:7000/
     # Use 0.0.0.0 to run in any IP available
-<<<<<<< HEAD
     app.run(host='0.0.0.0', port=7000)
-=======
-    app.run(host='0.0.0.0', port=7000, threaded=True)
->>>>>>> 7c4c5a9f80ca19a2024f33718e7f93d73dca4445
